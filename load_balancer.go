7ba55176e7545de1ae2c7aa9b139bb772f038bc0
hellomaxime <86252821+hellomaxime@users.noreply.github.com>
2024-02-21 17:21:48 +0100

    route requests to servers

--- load_balancer.go
+++ load_balancer.go
@@ -2,12 +2,22 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 )
 
+var i = 0;
+var servers = [3]int{8081, 8082, 8083}
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(r);
+	resp, _ := http.Get(fmt.Sprintf("http://localhost:%d", servers[i]))
+	i = (i + 1)%len(servers)
+	bodyBytes, err := io.ReadAll(resp.Body);
+	if(err != nil) {
+		log.Fatal(err)
+	}
+	fmt.Fprint(w, string(bodyBytes))
 } 
 
 func main() {
